a7c9ee950b6ad4856c4fe04e9fed83228960dddb
fran <filipinas.3@gmail.com>
2021-08-04 23:59:22 -0400

    Setting up environment for development

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
